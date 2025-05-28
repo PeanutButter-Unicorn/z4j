f0acc478481a7b5df8feb9e227cfb46844cb9eb9
jonathan zollinger <jonathan.zollinger@outlook.com>
2025-05-27 21:27:24 -0600

    fix(build): add missing parent dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     id("it.nicolasfarabegoli.conventional-commits") version "3.1.3"
     id("groovy")
-    id("io.micronaut.library") version "4.5.3" apply false
-    id("io.micronaut.aot") version "4.5.3" apply false
+    id("io.micronaut.library") version "4.5.3"
+    id("io.micronaut.aot") version "4.5.3"
     id("io.micronaut.openapi") version "4.5.3" apply false
 }
 
@@ -10,6 +10,45 @@ version = project.properties["z4jVersion"]!!
 group = "lol.pbu"
 
 allprojects { repositories { mavenCentral() } }
+
+dependencies {
+    annotationProcessor("io.micronaut.validation:micronaut-validation-processor")
+    implementation("io.micronaut.reactor:micronaut-reactor-http-client")
+    implementation("io.micronaut:micronaut-http-client")
+    implementation("io.micronaut.serde:micronaut-serde-jackson")
+    implementation("org.slf4j:jul-to-slf4j")
+    implementation("io.micronaut.validation:micronaut-validation")
+    runtimeOnly("ch.qos.logback:logback-classic")
+    runtimeOnly("com.fasterxml.jackson.module:jackson-module-kotlin")
+    runtimeOnly("org.yaml:snakeyaml")
+    testImplementation("org.testcontainers:spock")
+    testImplementation("org.testcontainers:testcontainers")
+    testImplementation("net.datafaker:datafaker:2.4.3")
+}
+java {
+    sourceCompatibility = JavaVersion.toVersion("17") // graalvm-ce
+}
+
+micronaut {
+    runtime("netty")
+    testRuntime("spock2")
+    processing {
+        incremental(true)
+        annotations("lol.pbu.*")
+    }
+    aot {
+        // Please review carefully the optimizations enabled below
+        // Check https://micronaut-projects.github.io/micronaut-aot/latest/guide/ for more details
+        optimizeServiceLoading = false
+        convertYamlToJava = false
+        precomputeOperations = true
+        cacheEnvironment = true
+        optimizeClassLoading = true
+        deduceEnvironment = true
+        optimizeNetty = true
+        replaceLogbackXml = true
+    }
+}
 
 subprojects {
     apply(plugin = "groovy")
