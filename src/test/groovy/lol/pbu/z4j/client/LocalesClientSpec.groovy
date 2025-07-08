c067eed431d89e631239551b2ea221f8e6175acf
jonathan zollinger <jonathan.zollinger@outlook.com>
2025-07-07 19:37:20 -0600

    docs: introduce contributing docs

--- src/test/groovy/lol/pbu/z4j/client/LocalesClientSpec.groovy
+++ src/test/groovy/lol/pbu/z4j/client/LocalesClientSpec.groovy
@@ -1,24 +1,52 @@
 package lol.pbu.z4j.client
 
+
 import io.micronaut.http.HttpStatus
+import io.micronaut.http.client.exceptions.HttpClientException
+import io.micronaut.http.client.exceptions.HttpClientResponseException
 import io.micronaut.test.extensions.spock.annotation.MicronautTest
-import jakarta.inject.Inject
 import spock.lang.Shared
-import spock.lang.Specification
+import spock.lang.Unroll
 
 @MicronautTest
-class LocalesClientSpec extends Specification {
+class LocalesClientSpec extends Z4jSpec {
 
-    @Inject
     @Shared
-    LocalesClient localesClient
+    LocalesClient managersLocalesClient, agentsLocalesClient, usersLocalesClient, noAuthLocalesClient,
+                  badEmailLocalesClient, badUrlLocalesClient
 
-    def "can list public locales"(){
+    def setupSpec() {
+        managersLocalesClient = managerCtx.getBean(LocalesClient.class)
+        agentsLocalesClient = agentCtx.getBean(LocalesClient.class)
+        usersLocalesClient = userCtx.getBean(LocalesClient.class)
+        noAuthLocalesClient = badTokenCtx.getBean(LocalesClient.class)
+        badEmailLocalesClient = badEmailCtx.getBean(LocalesClient.class)
+        badUrlLocalesClient = badUrlCtx.getBean(LocalesClient.class)
+    }
+
+
+    @Unroll
+    def "can list public locales for #clientName"() {
         when:
         def response = localesClient.listLocales()
 
         then:
         response.status() == HttpStatus.OK
+
+        where:
+        clientName              | localesClient
+        "managers client"       | managersLocalesClient
+        "agents client"         | agentsLocalesClient
+        "users client"          | usersLocalesClient
+        "no-auth client"        | noAuthLocalesClient
+        "bad email client"      | badEmailLocalesClient
     }
 
+    def "calling locales client with a bad URL throws an exception"() {
+        when:
+        badUrlLocalesClient.listLocales()
+
+        then:
+        thrown(HttpClientException)
+    }
 }
